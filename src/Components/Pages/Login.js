50358baae13b272406e00522ac579920cbdac8b5
Stephen Timphony <stevetimph@yahoo.com>
2021-03-17 11:53:48 -0500

    login component works properly. just need to organize and formatt it

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import {React, useState} from 'react';
+import {Form, Button} from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    function authenticate(event) {
+        
+    fetch('http://fitnesstrac-kr.herokuapp.com/api/users/login', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        })
+      }).then(response => response.json())
+        .then(result => {
+          console.log(result);
+        })
+        .catch(console.error);
+
+
+        event.preventDefault()
+    }
     return (
         <div className="home">
             Login
+            <Form>
+  <Form.Group controlId="Username">
+    <Form.Label>Username</Form.Label>
+    <Form.Control type="username" placeholder="Username" onChange={(event)=>setUsername(event.target.value)}
+                />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" onChange={(event)=>setPassword(event.target.value)} />
+  </Form.Group>
+    <Button variant="primary" type="submit" onClick={authenticate}>
+    Submit
+  </Button>
+</Form>
         </div>
     )
 
