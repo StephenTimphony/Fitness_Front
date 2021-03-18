5410e1e5592b6ad46d717934f6e3d098a4bbb3bc
ddelga85 <ddelga85@gmail.com>
2021-03-17 21:08:18 -0500

    working on myRoutines, mostly done

--- src/Components/Pages/MyRoutines.js
+++ src/Components/Pages/MyRoutines.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
+import { getUserPublicRoutines } from '../../api/index';
+import RoutineCard from '../RoutineCard';
+import NavBar from '../NavBar';
 
 
 
 const MyRoutines = () => {
+    const [ myRoutines, setMyRoutines ] = useState([]);
+    
+    useEffect (() => {
+        async function fetchData() {
+            const response = await getUserPublicRoutines()
+            setMyRoutines(response);
+        }
+        fetchData();
+    }, [])
+
     return (
-        <div className="home">
-        my Routines
+        <div>
+            <NavBar/> 
+        <div className="myRoutines">
+            {
+                myRoutines.map((routine, idx) => {
+                    console.log(routine)
+                    const { activities, creatorId, creatorName, goal, name } = routine
+                    return (
+                        <RoutineCard key={ idx } 
+                        activities={ activities }
+                        creatorId={ creatorId }
+                        creatorName={ creatorName }
+                        goal={ goal }
+                        name={ name}
+                        />
+                    )
+                })
+            }
+        </div>
         </div>
     )
-
-
-
 }
 
 export default MyRoutines;
+
+// id: 957
+// activities: []
+// creatorId: 1
+// creatorName: "albert"
+// goal: "Let's do this!"
+// id: 957
+// isPublic: true
+// name: "Hello Routine"
